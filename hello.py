7d397694a0e1b5dcfad61c5fd15a4c73a8bebbf7
SteveBenz <stbenz@microsoft.com>
2020-11-06 21:46:22 -0800

    Added symbols assets

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@
 import pygame.display as display
 import pygame.font as font
 import pygame.draw as draw
+import pygame.image as image
 import pygame as pygame
 from enum import Enum
  
@@ -20,21 +21,30 @@ class IntractableKind(Enum):
     Xor = "xor"
     Button = "button"
 
+class Assets:
+    def __init__(self):
+        self.and_ = image.load("assets/and-black.png")
+        self.or_ = image.load("assets/or-black.png")
+        self.xor = image.load("assets/xor-black.png")
+        self.nand = image.load("assets/nand-black.png")
+        self.nor = image.load("assets/nor-black.png")
+        self.xnor = image.load("assets/xnor-black.png")
+
 class Interactable:
-    def __init__(self, kind: IntractableKind, _screen: pygame.Surface, _font: font.Font, pos):
+    def __init__(self, kind: IntractableKind, screen: pygame.Surface, assets: Assets, pos):
         self.kind = kind
         self.isPowered = False
         self.connections = []
         self.pos = pos
-        self._font = _font
-        self._screen = _screen
+        self._assets = assets
+        self._screen = screen
     
     def draw(self):
-        gateText = self._font.render('A', True, RED)
-        gateRect = gateText.get_rect()
-        draw.rect(gateText, BLUE, gateRect, 2)
-        self._screen.blit(gateText, self.pos)
-
+        gateRect = self._assets.nor.get_rect() # gateRect is the size of the image...
+        gateRect.move_ip(self.pos) # now it's the rect moved to the spot we want it
+        draw.rect(self._screen, GRAY, gateRect)
+        draw.rect(self._screen, BLUE, gateRect, 4)
+        self._screen.blit(self._assets.nor, self.pos)
 
 
 
@@ -49,6 +59,7 @@ def main():
     pygame.display.set_icon(logo)
     pygame.display.set_caption("minimal program")
 
+
     sysfont = font.SysFont(None, 24)
 
     # create a surface on screen that has the size of 240 x 180
@@ -56,6 +67,7 @@ def main():
      
     # define a variable to control the main loop
     running = True
+    assets = Assets()
 
     interacables = []
      
@@ -64,7 +76,7 @@ def main():
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                interacables.append( Interactable(IntractableKind.And, screen, sysfont, event.pos) )
+                interacables.append( Interactable(IntractableKind.And, screen, assets, event.pos) )
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
---
+++ todo.md
@@ -0,0 +1,14 @@
+# Todo List
+
+[ ]  Make the window reasonably sized
+[ ]  Object is placed centered on the mouse, not Upper-Left
+[ ]  Fix it so that the highlight box doesn't overlap the thing
+[ ]  Select, not just place
+[ ]  Create a region that's used for control (and you can't place stuff in that region)
+[ ]  Change gate types
+[ ]  Delete gates
+[ ]  Draw connections
+[ ]  Create an Input widget
+[ ]  Input widget can be on, off, turning on or turning off
+[ ]  Gates show whether they're currently outputting true or false
+[ ]  Add Step and Reset buttons to control area
